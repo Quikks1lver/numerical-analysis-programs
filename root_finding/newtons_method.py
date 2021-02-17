f311a17b14d0284a191dd65073b471496d5c02a1
Adam Fernandes <fernadam8@gmail.com>
2021-02-17 17:59:46 -0500

    secant

--- root_finding/newtons_method.py
+++ root_finding/newtons_method.py
@@ -1,9 +1,26 @@
 # Adam Fernandes
 # February 2021
-# Newton's Method, Secant Method, and False Position Method for finding roots
+# Newton's Method, Secant Method for finding roots
 
 from math import cos, sin, tan, pi
 from helpers import convertDegreeToRadian
+from typing import List
+
+class Val:
+   """
+   A class to represent past values
+   """
+   def __init__(self, val: float):
+      self.val = val
+
+   def isPositive(self) -> bool:
+      """
+      Returns True if positive, False otherwise
+      """
+      return True if self.val > 0 else False
+   
+   def __repr__(self):
+      return f"{self.val}"
 
 def f(x: float) -> float:
    """
@@ -21,6 +38,8 @@ def newtonsMethod(p0: float, tolerance: float, maxIterations: int, moreOutput: b
    """
    Undergoes Newton's method, you need to do the function operations though
    """
+   print(f"\t--- Newton's Method ---\n")
+
    numIterations: int = 1
    prev, px, diff = p0, p0, p0
 
@@ -53,6 +72,51 @@ def newtonsMethod(p0: float, tolerance: float, maxIterations: int, moreOutput: b
    
    return
 
+def secantMethod(p0: float, p1: float, tolerance: float, maxIterations: int, moreOutput: bool, falsePosition: bool) -> None:
+   """
+   Undergoes secant method, you need to do the function operations though
+   Haven't implemented false position (yet)
+   """
+   print(f"\t--- Secant Method ---\n")
+
+   numIterations: int = 1
+   pastVals: List[Val] = []
+   
+   pastVals.append(Val(p0))
+   pastVals.append(Val(p1))
+
+   diff = p1
+   px = 0
+
+   while diff >= tolerance and numIterations <= maxIterations:
+      prev = px
+
+      try:
+         prev1 = pastVals[-1]
+         prev2 = pastVals[-2]
+
+         px = prev1.val - ((f(prev1.val) * (prev1.val - prev2.val)) / (f(prev1.val) - f(prev2.val)))
+         px_val = Val(px)
+         pastVals.append(px_val)
+         
+      except:
+         print(f"\n\tSomething wack happened at iteration {numIterations}. Caught error.\n")
+         return
+
+      if moreOutput:
+         print(f"\tIteration #{numIterations} -- {round(px, 5)} <- f({round(prev, 3)})")
+
+      diff = abs(px - prev)
+      numIterations += 1
+   
+   numIterations -= 1 # account for last for while loop increment
+   if numIterations >= maxIterations:
+      print(f"\n\tExceeded max iterations ({maxIterations}), yielded {px}\n")
+   else:
+      print(f"\n\tAfter {numIterations} iterations -- {px}\n")
+   
+   return
+
 def main():
    print("\n\tRemember to change function!\n")
 
@@ -61,7 +125,11 @@ def main():
    maxIterations: int = 10
    moreOutput: bool = True
 
-   newtonsMethod(p0, tolerance, maxIterations, moreOutput) # CHANGE THE FUNCTION F FROM ABOVE TO TEST!!
+   # newtonsMethod(p0, tolerance, maxIterations, moreOutput) # CHANGE THE FUNCTION F FROM ABOVE TO TEST!!
+   
+   p0 = 3
+   p1 = 2
+   secantMethod(p0, p1, tolerance, 10, True, False)
 
 if __name__ == "__main__":
    main()
