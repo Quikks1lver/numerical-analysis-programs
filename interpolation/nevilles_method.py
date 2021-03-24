e4d923a38772441e7dd7c95764e5a8765e105e60
Adam Fernandes <fernadam8@gmail.com>
2021-03-24 17:46:13 -0400

    cite

--- interpolation/nevilles_method.py
+++ interpolation/nevilles_method.py
@@ -1,6 +1,7 @@
 # Adam Fernandes
 # March 2021
 # Utilize's Neville's method to help with interpolation
+# Coded using Dr. Glinos's code from UCF, from Dr. Jahani's num calc slides
 
 from typing import List
 
