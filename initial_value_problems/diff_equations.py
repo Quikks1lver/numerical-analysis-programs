96b2be3a3d7c0119a6e9ef7caf1eade0d89b8dd3
Adam Fernandes <fernadam8@gmail.com>
2021-04-27 09:25:40 -0400

    unlimited calculations

--- initial_value_problems/diff_equations.py
+++ initial_value_problems/diff_equations.py
@@ -2,20 +2,31 @@
 # April 2021
 # Simple program for easily getting input/output for f(t,y) differential equations
 
-def equation(t: float, y: float) -> float:
+from typing import Union
+
+STOP_VAL: int = -1000
+
+def equation(t: float, y: float) -> Union[float, None]:
    """
    Define this yourself!
    """
-   return (y/t) - (y/t)**2
+   try:
+      return (y/t) - (y/t)**2
+   except:
+      print("\n\tERROR with calculation")
 
 def main():
-   print("Input t-value")
-   t = float(input())
-   
-   print("Input y-value")
-   y = float(input())
+   while True:
+      print(f"Input t-value (then ENTER) and y-value (then ENTER). {STOP_VAL} for either to quit.")
+      
+      t = float(input())
+      if t == STOP_VAL:
+         break
+      y = float(input())
+      if y == STOP_VAL:
+         break
 
-   print(f"\n\tEquation evaluated at t = {t}, y = {y}\t=\t{equation(t, y)}\n")
+      print(f"\n\tEquation evaluated at t = {t}, y = {y}\t=\t{equation(t, y)}\n")
 
 if __name__ == "__main__":
    main()
